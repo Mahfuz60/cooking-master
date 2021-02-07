b7613b21aa0467ce159937b3de6c299461194dff
Mahfuz <you@example.com>
2021-02-07 19:14:52 +0600

    finally done

--- app.js
+++ app.js
@@ -1,100 +1,84 @@
-const button = document.getElementById('searchButton');
-const mealPlace = document.getElementById('meal');
-const resultPlace= document.getElementById('showResult');
+//calling API
+const inputMeal = document.getElementById("search-field");
+const button = document.getElementById("search-btn");
+const displayArea = document.getElementById("display");
+const detailsArea = document.getElementById("details-area");
+const mealUrl = "https://www.themealdb.com/api/json/v1/1/";
 
-button.addEventListener('click', (event) => {
-    event.preventDefault();
-    const userInput = document.getElementById('inputForSearch').value;
-    loadData(userInput);
+//take input
+button.addEventListener("click", () => {
+  searchFoodByName(inputMeal.value);
 });
-
-function loadData(userInput) {
-
-    let url = "";
-    if (userInput.length === 1) {
-        url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${userInput}`;
-        mealPlace.innerHTML = null;
-        resultPlace.innerHTML = null;
-
-    } else {
-        url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`;
-        mealPlace.innerHTML = null;
-        resultPlace.innerHTML = null;
-    }
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            displayData(data)
-        })
-    //.catch(error => console.log(error))
-}
-
-
-
-const displayData = data => {
-
-    data.meals.forEach(meal => {
-
-        const mealDiv = document.createElement('div');
-
-        const mealIntro = `
-        <div  class="col">
-            <div class="card h-100">
-                <img  class="card-img-top" src="${meal.strMealThumb}"/>
-                <div class="card-body">
-                    <h5 class="card-title mx-auto">${meal.strMeal}</h5>
-                    
+const searchFoodByName = (keyword) => {
+  if (keyword != "") {
+    showLoader(displayArea, true);
+    let url = `${mealUrl}search.php?s=${keyword}`;
+    fetch(encodeURI(url))
+      .then((data) => data.json())
+      .then((data) => {
+        showLoader(displayArea, false);
+        displayFood(data);
+      });
+  }
+};
+const displayFood = (data) => {
+  if (data.meals == null) {
+    showNotFoundMessage();
+  } else {
+    displayArea.innerHTML = createFoodCard(data);
+  }
+};
+//if noting match through search
+const showNotFoundMessage = () => {
+  displayArea.innerHTML = `<h1>Sorry,Your Food is Not Available):</h1>`;
+};
+const createFoodCard = (data) => {
+  let meals = data.meals;
+  let elementString = "";
+  meals.forEach((data) => {
+    elementString += `<div class="food-item" onclick="showFoodDetails(${data.idMeal})">
+                <div class="thumbnail">
+                    <img src="${data.strMealThumb}"/>
                 </div>
-            </div>
-        </div> `;
-        mealDiv.innerHTML = mealIntro;
-        mealPlace.appendChild(mealDiv);
-
+                <div class="food-name">
+                    <h3>${data.strMeal}</h3>
+                </div>
+            </div>`;
+  });
+  return elementString;
+};
+const showFoodDetails = (id) => {
+  let url = `${mealUrl}lookup.php?i=${id}`;
+  fetch(encodeURI(url))
+    .then((data) => data.json())
+    .then((data) => {
+      let item = data.meals[0];
+      let ingredients = "";
+      for (let i = 1; i <= 6; i++) {
+        ingredients += `<li><i class="material-icons">check_box</i> ${
+          item["strIngredient" + i]
+        }</li>`;
+      }
+      detailsArea.innerHTML = `<section id="modal">
+            <div class="modal-content">
+                <div class="modal-body">
+                  <div class="food-details">
+                    <button id="modal-btn" onclick="hideFoodDetails()">X</button>
+                    <img src="${item.strMealThumb}" />
+                    <div class="details">
+                      <h1>${item.strMeal}</h1>
+                      <h4>Ingredients</h4>
+                      <ul>${ingredients}</ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>`;
     });
-}
-
-const displayMealDetails = (string) => {
-
-    url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${string}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-
-            console.log('JUST STRING NAME : ', string);
-
-            resultPlace.style.display = "block"
-            const div = document.createElement('div');
-
-            let meal;
-            let mealInfo;
-            for (let i = 0; i < data.meals.length; i++) {
-                meal = data.meals[i];
-
-                if (string === meal.strMeal) {
-                    mealInfo = `
-                    <img src="${meal.strMealThumb}" class="card-img-top">
-                    <div class="card-body">
-                    <h3 class="card-title">${meal.strMeal}</h3>
-                    <p>Ingredients</p>
-                    <ul>                    
-                        <li>${meal.strIngredient1}</li>
-                        <li>${meal.strIngredient2}</li>
-                        <li>${meal.strIngredient3}</li>
-                        <li>${meal.strIngredient4}</li>
-                        <li>${meal.strIngredient5}</li>
-                        <li>${meal.strIngredient6}</li>
-                        <li>${meal.strIngredient7}</li>
-                        <li>${meal.strIngredient8}</li>
-                        <li>${meal.strIngredient9}</li>
-                        <li>${meal.strIngredient10}</li>
-                    </ul>
-                    </div> `;
-                }
-            }
-
-            mealDiv.innerHTML = mealInfo;
-            resultPlace.appendChild(mealDiv);
-        });
-    
-    resultPlace.innerHTML = null;
-}
+};
+const hideFoodDetails = () => {
+  detailsArea.innerHTML = "";
+};
+const showLoader = (parent, argument) => {
+  argument ? (parent.innerHTML = `<div class="loader"></div>`) : "";
+};
